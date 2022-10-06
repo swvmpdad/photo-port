059d42a6182ea38016db8d3bd08bccba8c0e31a7
swvmpdad <will.g.camp@gmail.com>
2022-10-06 18:28:32 -0500

    created form submit functions

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    console.log(formState);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return(
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
-                // name input
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
 
-                // email input
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange}  name="email" />
                 </div>
 
-                // message text area
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <input type="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
